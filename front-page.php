f2a8d78ddc95bc41b07c7b05605863f8bad8b1bd
your_name <your_email_id>
2024-05-23 18:04:30 +0300

    slider 23.05

--- front-page.php
+++ front-page.php
@@ -8,24 +8,30 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="10000">
-            <img src="https://user-images.githubusercontent.com/78242022/284473812-eda73791-83d8-47db-bb4f-21de64621094.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+            <div class="main-slider_slide main-slider_slide--vint">
+                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xcompressor-collage.png.pagespeed.ic.CxEZCQdSGh.webp" class="d-block" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
             </div>
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-            <img src="https://user-images.githubusercontent.com/78242022/284473830-babc90af-7e73-4ee8-813b-7d87c4fae908.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
+            <div class="main-slider_slide main-slider_slide--vint">
+                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xvintovoy.png.pagespeed.ic.z_e06HpYGB.webp" class="d-block" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="https://user-images.githubusercontent.com/78242022/284473823-1d713ee7-af6a-4913-b4e4-01d2d78c7e2d.jpg " class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
+            <div class="main-slider_slide main-slider_slide--vint">
+                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xporshnevoy.png.pagespeed.ic.H1bXZ0iBlc.webp" class="d-block" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
             </div>
         </div>
     </div>
