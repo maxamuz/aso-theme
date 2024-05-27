0cce1b11ee1c4bc4396770e2c2d702b3c729a751
Alexander <laptevx@dmail.com>
2024-05-27 14:08:52 +0300

    header bottom line

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" data-bs-interval="false"
+<div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false"
     data-bs-pause="hover">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
@@ -16,7 +16,8 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="slider-text">
-                                <p class="h5">Поршневые и винтовые компрессоры АСО - лучшее предложение на российском
+                                <p class="slider-text-header">Поршневые и винтовые компрессоры АСО - лучшее предложение
+                                    на российском
                                     рынке</p>
                                 <a href="/product-category/vintovye-kompressory/"
                                     class="main-slider_button catalog-button--blue">
@@ -37,21 +38,49 @@
         </div>
         <div class="carousel-item" data-bs-interval="2000">
             <div class="main-slider_slide main-slider_slide--vint">
-                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xvintovoy.png.pagespeed.ic.z_e06HpYGB.webp"
-                    class="d-block" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
+                <div class="container slider-container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="slider-text">
+                                <p class="slider-text-header">Надежные установки для производства сжатого воздуха в
+                                    промышленных масштабах</p>
+                                <a href="/product-category/vintovye-kompressory/"
+                                    class="main-slider_button catalog-button--blue">
+                                    <span class="button-body">Подробнее</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="img-container">
+                                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xvintovoy.png.pagespeed.ic.z_e06HpYGB.webp"
+                                    class="d-block" alt="...">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="carousel-item">
             <div class="main-slider_slide main-slider_slide--vint">
-                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xporshnevoy.png.pagespeed.ic.H1bXZ0iBlc.webp"
-                    class="d-block" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                <div class="container slider-container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="slider-text">
+                                <p class="slider-text-header">Удобный, доступный и простой источник сжатого воздуха для
+                                    циклических работ</p>
+                                <a href="/product-category/vintovye-kompressory/"
+                                    class="main-slider_button catalog-button--blue">
+                                    <span class="button-body">Подробнее</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="img-container">
+                                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xporshnevoy.png.pagespeed.ic.H1bXZ0iBlc.webp"
+                                    class="d-block" alt="...">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- header.php
+++ header.php
@@ -100,20 +100,23 @@
 
 
 			</div><!-- .site-branding -->
-			<div class="container">
-				<nav id="site-navigation" class="main-navigation">
-					<button class="menu-toggle" aria-controls="primary-menu"
-						aria-expanded="false"><?php esc_html_e('Primary Menu', 'aso-theme'); ?>
-					</button>
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'menu-1',
-							'menu_id' => 'primary-menu',
-						)
-					);
-					?>
-				</nav><!-- #site-navigation -->
+			<div class="main-menu">
+				<div class="container">
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu"
+							aria-expanded="false"><?php esc_html_e('Primary Menu', 'aso-theme'); ?>
+						</button>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_id' => 'primary-menu',
+							)
+						);
+						?>
+					</nav><!-- #site-navigation -->
+				</div>
 			</div>
+
 
 		</header><!-- #masthead -->
