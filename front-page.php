7769b3f1961726d5e0b1fa92acc03d72170d5eee
Alexander <laptevx@dmail.com>
2024-05-28 18:47:18 +0300

    custom sields slider

--- front-page.php
+++ front-page.php
@@ -9,81 +9,34 @@
         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
+        <?php $fields = CFS()->get('slider'); ?>
 
-            <div class="main-slider_slide main-slider_slide--vint">
-                <div class="container slider-container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="slider-text">
-                                <p class="slider-text-header">Поршневые и винтовые компрессоры АСО - лучшее предложение
-                                    на российском
-                                    рынке</p>
-                                <a href="/product-category/vintovye-kompressory/"
-                                    class="main-slider_button catalog-button--blue">
-                                    <span class="button-body">Подробнее</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="img-container">
-                                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xcompressor-collage.png.pagespeed.ic.CxEZCQdSGh.webp"
-                                    class="d-block" alt="...">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <?php if (!empty($fields)): ?>
 
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <div class="main-slider_slide main-slider_slide--vint">
-                <div class="container slider-container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="slider-text">
-                                <p class="slider-text-header">Надежные установки для производства сжатого воздуха в
-                                    промышленных масштабах</p>
-                                <a href="/product-category/vintovye-kompressory/"
-                                    class="main-slider_button catalog-button--blue">
-                                    <span class="button-body">Подробнее</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="img-container">
-                                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xvintovoy.png.pagespeed.ic.z_e06HpYGB.webp"
-                                    class="d-block" alt="...">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="main-slider_slide main-slider_slide--vint">
-                <div class="container slider-container">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="slider-text">
-                                <p class="slider-text-header">Удобный, доступный и простой источник сжатого воздуха для
-                                    циклических работ</p>
-                                <a href="/product-category/vintovye-kompressory/"
-                                    class="main-slider_button catalog-button--blue">
-                                    <span class="button-body">Подробнее</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="img-container">
-                                <img src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/slider/xporshnevoy.png.pagespeed.ic.H1bXZ0iBlc.webp"
-                                    class="d-block" alt="...">
+            <?php $i = 0;
+            foreach ($fields as $field): ?>
+                <div class="carousel-item<?php if ($i === 0)
+                    echo ' active'; ?>" data-bs-interval="10000">
+                    <div class="main-slider_slide main-slider_slide--vint">
+                        <div class="container slider-container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="slider-text">
+                                        <p class="slider-text-header"><?php echo $field['title']; ?></p>
+                                        <a href="<?php echo $field['href']; ?>" class="main-slider_button catalog-button--blue">
+                                            <span class="button-body">Подробнее</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="img-container"><?php echo $field['img']; ?></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <?php $i++; endforeach; ?>
+        <?php endif; ?>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -94,5 +47,6 @@
         <span class="visually-hidden">Next</span>
     </button>
 </div>
+
 
 <div class="container"><?php get_footer(); ?></div>
