1702f393803f9edd43e1587fd6c6c1444c959358
Alexander <laptevx@dmail.com>
2024-05-30 15:46:13 +0300

    header for block categories

--- front-page.php
+++ front-page.php
@@ -22,7 +22,7 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="slider-text">
-                                        <p class="slider-text-header"><?php echo $field['title']; ?></p>
+                                        <p class="text-h5"><?php echo $field['title']; ?></p>
                                         <a href="<?php echo $field['href']; ?>" class="main-slider_button catalog-button--blue">
                                             <span class="button-body">Подробнее</span>
                                         </a>
@@ -51,25 +51,68 @@
 <section class="container-fluid container-background">
     <section class="container">
         <div class="row">
-            <div class="col-6">
+            <div class="section_headline">Каталог Бежецких компрессоров</div>
+            <div class="col-md-6 col-sm-12">
                 <div class="group-items">
                     <a href="/product-category/porshnevye-kompressory/" class="group_item group_anime">
                         <div class="group_item_name">
-                            Поршневые компрессоры
-                            <p>Удобный, доступный и простой источник сжатого воздуха для циклических работ</p>
+                            <p class="text-h5">Поршневые компрессоры</p>
+                            <p class="description-text">Удобный, доступный и простой источник сжатого воздуха для
+                                циклических работ</p>
                         </div>
-                        <div class="group_item_short-desc"></div>
                         <div class="group_item_image">
                             <img
                                 src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/cats-on-main/xporsh2.png.pagespeed.ic.tSX51ykQEG.webp">
                         </div>
                     </a>
                 </div>
-
             </div>
-            <div class="col-6">2</div>
-            <div class="col-6">3</div>
-            <div class="col-6">4</div>
+            <div class="col-md-6 col-sm-12">
+                <div class="group-items">
+                    <a href="/product-category/porshnevye-kompressory/" class="group_item group_anime">
+                        <div class="group_item_name">
+                            <p class="text-h5">Винтовые компрессоры</p>
+                            <p class="description-text">Надежные установки для производства воздуха в промышленных
+                                масштабах</p>
+                        </div>
+                        <div class="group_item_image">
+                            <img
+                                src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/cats-on-main/xvint2.png.pagespeed.ic.Kjxv5JWINs.webp">
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="group-items">
+                    <a href="/product-category/porshnevye-kompressory/" class="group_item group_anime">
+                        <div class="group_item_name">
+                            <p class="text-h5">Подготовка воздуха</p>
+                            <p class="description-text">Оптимальные и выгодные решения для очистки поверхностей любой
+                                площади</p>
+                        </div>
+                        <div class="group_item_image">
+                            <img
+                                src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/cats-on-main/xresiver2.png.pagespeed.ic.lnF0ZZM6Ta.webp">
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="group-items">
+                    <a href="/product-category/porshnevye-kompressory/" class="group_item group_anime">
+                        <div class="group_item_name">
+                            <p class="text-h5">Пескоструйное оборудование</p>
+                            <p class="description-text">Удобный, доступный и простой источник сжатого воздуха для
+                                циклических работ</p>
+                        </div>
+                        <div class="group_item_short-desc"></div>
+                        <div class="group_item_image">
+                            <img
+                                src="https://asobezh-msk.ru/wp-content/themes/aco-child/src/img/cats-on-main/xpeskostruika2.png.pagespeed.ic.5aDltOxcj2.webp">
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
 
     </section>
--- functions.php
+++ functions.php
@@ -150,7 +150,13 @@ function aso_theme_scripts()
 	wp_style_add_data('aso-theme-style', 'rtl', 'replace');
 
 	wp_enqueue_script('aso-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
-	wp_enqueue_script('aso-theme-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', true);
+	wp_enqueue_script(
+		'aso-theme-bootstrap',
+		'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+		array(),
+		_S_VERSION,
+		true
+	);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
